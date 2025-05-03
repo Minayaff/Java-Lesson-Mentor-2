3eb6024401052b770f760936738abd9a8d2ed868
minayaff <minayaff@code.edu.az>
2025-05-03 10:57:39 +0400

    student object create

---
+++ src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    String name;
+    String surname;
+    byte age;
+    String email;
+
+    public void getFullname(){
+        System.out.println("Full Name is "+name +" "+surname);
+    }
+}
+
